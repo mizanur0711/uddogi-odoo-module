b9caf646577322ed9473057f845ae2fa1d87d0e7
mizanur0711 <mizanur0711@gmail.com>
2024-08-16 12:12:44 +0600

    Modules published

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'VATSync - Uddogi for Odoo [Bangladesh]',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'summary': 'Here odoo will send structured data for Uddogi throug API',
     'author': 'Nascenia Limited',
     'category': 'Sales',
